b137ab8cdaaa7d72a86c1ea48a5461dd2573be40
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2023-09-05 02:26:13 +0200

    some lint fixed

--- src/components/Map/createBaseMap.jsx
+++ src/components/Map/createBaseMap.jsx
@@ -7,25 +7,34 @@ import MarkerClusterGroup from 'react-leaflet-cluster';
 import { LocationControl } from './components/LocationControl/LocationControl';
 import { mapConfig } from './map.config';
 
+// Define PropTypes for the object shape inside the array
+const markerPropTypes = PropTypes.shape({
+  // Define the properties you expect in each marker object
+  // For example:
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  // Add more properties as needed
+});
+
 export const MapComponent = ({ markers }) => (
-    <MapContainer
-        center={mapConfig.initialMapCoordinates}
-        zoom={mapConfig.initialMapZoom}
-        scrollWheelZoom
-        style={{ height: '100%' }}
-    >
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&amp;copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-            maxZoom={mapConfig.maxMapZoom}
-        />
+  <MapContainer
+    center={mapConfig.initialMapCoordinates}
+    zoom={mapConfig.initialMapZoom}
+    scrollWheelZoom
+    style={{ height: '100%' }}
+  >
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&amp;copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      maxZoom={mapConfig.maxMapZoom}
+    />
 
-        <MarkerClusterGroup>{markers}</MarkerClusterGroup>
+    <MarkerClusterGroup>{markers}</MarkerClusterGroup>
 
-        <LocationControl />
-    </MapContainer>
+    <LocationControl />
+  </MapContainer>
 );
 
 MapComponent.propTypes = {
-    markers: PropTypes.array.isRequired,
+  markers: PropTypes.arrayOf(markerPropTypes).isRequired, // Use PropTypes.arrayOf() with the markerPropTypes
 };
