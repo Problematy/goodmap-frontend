37c88b83e220bbffd7e836d0faee3a69c2367c99
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-08-29 18:46:54 +0200

    refactored

--- src/components/Map/createBaseMap.jsx
+++ src/components/Map/createBaseMap.jsx
@@ -1,32 +1,32 @@
-
 import React, { useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import PropTypes from 'prop-types';
-import { LocationControl, LocationButton } from './components/LocationControl/LocationControl';
+import { LocationControl } from './components/LocationControl/LocationControl';
 import { mapConfig } from './map.config';
 
 export const MapComponent = ({ markers }) => {
     const [userPosition, setUserPosition] = useState(null);
-    return <MapContainer
-        center={mapConfig.initialMapCoordinates}
-        zoom={mapConfig.initialMapZoom}
-        scrollWheelZoom
-        style={{ height: '100%' }}
-    >
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&amp;copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-            maxZoom={mapConfig.maxMapZoom}
-        />
 
-<MarkerClusterGroup>
-    {markers.map((marker, index) => React.cloneElement(marker, { key: index }))}
-</MarkerClusterGroup>
-        <LocationControl setUserPosition={setUserPosition} />
-    </MapContainer>
-    }
-
+    return (
+        <MapContainer
+            center={mapConfig.initialMapCoordinates}
+            zoom={mapConfig.initialMapZoom}
+            scrollWheelZoom
+            style={{ height: '100%' }}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&amp;copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                maxZoom={mapConfig.maxMapZoom}
+            />
+            <MarkerClusterGroup>
+                {markers.map((marker, index) => React.cloneElement(marker, { key: index }))}
+            </MarkerClusterGroup>
+            <LocationControl setUserPosition={setUserPosition} />
+        </MapContainer>
+    );
+}
 
 MapComponent.propTypes = {
     markers: PropTypes.arrayOf(
