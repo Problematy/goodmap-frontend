6ed773a8d8229c42510463da8ff93aafd46b3661
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-08-30 02:29:02 +0200

    updated testing library

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const config = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     collectCoverageFrom: ['src/components/**/*.jsx'],
+    transformIgnorePatterns: [
+        'node_modules/(?!(react-leaflet|@react-leaflet/core)/)',
+    ]
 };
 
 module.exports = config;
