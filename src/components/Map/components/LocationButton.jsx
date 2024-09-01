028d4c02b638dfba0528d86b7c280d37b8f98ed5
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-09-01 21:37:13 +0200

    common button style

--- src/components/Map/components/LocationButton.jsx
+++ src/components/Map/components/LocationButton.jsx
@@ -4,26 +4,7 @@ import PropTypes from 'prop-types';
 import Control from 'react-leaflet-custom-control';
 import { Button, Box } from '@mui/material';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
-
-const buttonStyle = {
-    width: '50px',
-    height: '50px',
-    minWidth: '50px',
-    borderRadius: '50%',
-    backgroundColor: '#3d91e5',
-    color: 'white',
-    fontSize: '24px',
-    textAlign: 'center',
-    cursor: 'pointer',
-    transition: 'background-color 0.2s ease-in-out',
-    border: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '0',
-    lineHeight: '1',
-    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-};
+import { buttonStyle } from '../../../styles/buttonStyle';
 
 export const LocationButton = ({ userPosition = null }) => {
     const map = useMap();
--- src/components/Map/components/SuggestNewPointButton.jsx
+++ src/components/Map/components/SuggestNewPointButton.jsx
@@ -3,29 +3,9 @@ import Modal from 'react-modal';
 import { Button, Box, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Snackbar } from '@mui/material';
 import axios from 'axios';
 import Control from 'react-leaflet-custom-control';
-import AddIcon from '@mui/icons-material/Add'; // Import ikony dla przycisku dodawania punktu
+import AddIcon from '@mui/icons-material/Add';
+import { buttonStyle } from '../../../styles/buttonStyle';
 
-const buttonStyle = {
-    width: '50px',
-    height: '50px',
-    minWidth: '50px',
-    borderRadius: '50%',
-    backgroundColor: '#3d91e5',
-    color: 'white',
-    fontSize: '24px',
-    textAlign: 'center',
-    cursor: 'pointer',
-    transition: 'background-color 0.2s ease-in-out',
-    border: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '0',
-    lineHeight: '1',
-    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-};
-
-// Komponent dla przycisku dodawania nowego punktu
 export const SuggestNewPointButton = () => {
     const [showModal, setShowModal] = useState(false);
     const [issue, setIssue] = useState('');
