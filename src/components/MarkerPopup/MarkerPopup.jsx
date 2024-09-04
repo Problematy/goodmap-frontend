04c28b6f1312483a82c34e407d89be7077676da7
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-09-05 00:03:01 +0200

    added way of running app broadcasting to network

--- src/components/MarkerPopup/MarkerPopup.jsx
+++ src/components/MarkerPopup/MarkerPopup.jsx
@@ -44,7 +44,8 @@ export const MarkerContent = ({ place }) => {
                 <p className="point-subtitle mt-0 mb-2">{place.subtitle}</p>
                 {categoriesWithSubcategories.map(mapDataToPopupContent)}
             </div>
-            <a href={`geo:${place.position[0]},${place.position[1]}?q=${place.position[0]},${place.position[1]}(${place.title})`}>
+            <a href={`geo:${place.position[0]},${place.position[1]}?q=${place.position[0]},${place.position[1]}`}
+            style={{ textDecoration: 'none' }}>
                 <p
                     style={{
                         ...buttonStyleSmall,
