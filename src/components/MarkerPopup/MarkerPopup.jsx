338acadccebd494c38846e2141511b2e80107edc
Dawid Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-11-14 12:18:07 +0100

    Remove debug print
    Co-authored-by: ravenwing <kolodzinski.krzys@gmail.com>

--- src/components/MarkerPopup/MarkerPopup.jsx
+++ src/components/MarkerPopup/MarkerPopup.jsx
@@ -58,7 +58,6 @@ export const MarkerContent = ({ place }) => {
     const CTACategories = place.data.filter(([category]) => category === 'CTA');
     const [showForm, setShowForm] = useState(false);
     const toggleForm = () => setShowForm(!showForm);
-    console.log(isMobile);
     return (
         <>
             <div className="place-data m-0">
