b4a0372afa77a9084b4bb3d91c6be875b23bab7f
Krzysztof <krzysztof.kolodzinski@problematy.pl>
2024-11-17 21:10:57 +0100

    mobile popup

--- src/components/MarkerPopup/MobilePopup.jsx
+++ src/components/MarkerPopup/MobilePopup.jsx
@@ -1,38 +1,45 @@
 import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import React from 'react';
+import React, { useState } from 'react';
 
-export const MobilePopup = ({ isOpen, onCloseHandler, children }) => (
-    <Dialog
-        open={isOpen}
-        onClose={onCloseHandler}
-        fullWidth
-        maxWidth="md"
-        style={{
-            position: 'fixed',
-            bottom: 0,
-            margin: 0,
-        }}
-        PaperProps={{
-            style: {
+export const MobilePopup = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const onCloseHandler = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <Dialog
+            open={isOpen}
+            onClose={onCloseHandler}
+            fullWidth
+            maxWidth="md"
+            style={{
                 position: 'fixed',
                 bottom: 0,
                 margin: 0,
-                width: '100%',
-                maxHeight: '50%',
-            },
-        }}
-    >
-        <DialogTitle>
-            <IconButton
-                aria-label="close"
-                onClick={onCloseHandler}
-                style={{ position: 'absolute', right: 8, top: 8 }}
-            >
-                <CloseIcon />
-            </IconButton>
-        </DialogTitle>
-
-        <DialogContent>{children}</DialogContent>
-    </Dialog>
-);
+            }}
+            PaperProps={{
+                style: {
+                    position: 'fixed',
+                    bottom: 0,
+                    margin: 0,
+                    width: '100%',
+                    maxHeight: '50%',
+                },
+            }}
+        >
+            <DialogTitle>
+                <IconButton
+                    aria-label="close"
+                    onClick={onCloseHandler}
+                    style={{ position: 'absolute', right: 8, top: 8 }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>{children}</DialogContent>
+        </Dialog>
+    );
+};
