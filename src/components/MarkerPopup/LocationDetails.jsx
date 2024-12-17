afe9bc756d34479f86c013298cac643faec3f9e0
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-12-17 14:01:08 +0100

    Remove react element from propTypes

--- src/components/MarkerPopup/LocationDetails.jsx
+++ src/components/MarkerPopup/LocationDetails.jsx
@@ -26,7 +26,6 @@ LocationDetailsValue.propTypes = {
         PropTypes.string,
         PropTypes.number,
         PropTypes.array,
-        PropTypes.element,
         PropTypes.shape({
             type: PropTypes.string.isRequired,
             value: PropTypes.string.isRequired,
@@ -61,7 +60,7 @@ const LocationDetails = ({ place }) => {
     const categoriesWithSubcategories = place.data.filter(([category]) => !(category === 'CTA'));
     // TODO CTA should be handled like website is
     const CTACategories = place.data.filter(([category]) => category === 'CTA');
-
+    console.log(CTACategories);
     return (
         <div className="place-data m-0">
             <div style={{ width: '100%', justifyContent: 'center', display: 'flex' }}>
