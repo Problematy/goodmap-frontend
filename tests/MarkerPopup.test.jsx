fcbfe361c78b7e0b2291b581e4aeb0a80af4362f
Martyna Szymańska <jamczylija@gmail.com>
2025-02-13 22:00:49 +0100

    Added fetch mocking (not working yet)

--- tests/MarkerPopup.test.jsx
+++ tests/MarkerPopup.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import { MapContainer } from 'react-leaflet';
 import { MarkerPopup } from '../src/components/MarkerPopup/MarkerPopup';
+import { httpService } from '../src/services/http/httpService';
 
-const correctMarkerData = {
+jest.mock('../src/services/http/httpService');
+
+const location = {
+    position: [51.1095, 17.0525],
+    name: 'name',
+    uuid: '21231',
+};
+
+const locationData = {
     title: 'Most Grunwaldzki',
     position: [51.1095, 17.0525],
     subtitle: 'big bridge',
@@ -27,18 +36,30 @@ const correctMarkerData = {
     },
 };
 
+httpService.getLocation.mockResolvedValue(locationData);
+
 describe('MarkerPopup', () => {
     beforeEach(() => {
-        render(
-            <MapContainer
-                center={[51.1095, 17.0525]}
-                zoom={10}
-                style={{ height: '100vh', width: '100%' }}
-            >
-                <MarkerPopup place={correctMarkerData} key={correctMarkerData.metadata.UUID} />
-            </MapContainer>,
+        jest.spyOn(global, 'fetch').mockResolvedValue({
+            json: jest.fn().mockResolvedValue(locationData),
+        });
+        return act(() =>
+            render(
+                <MapContainer
+                    center={[51.1095, 17.0525]}
+                    zoom={10}
+                    style={{ height: '100vh', width: '100%' }}
+                >
+                    <MarkerPopup place={location} key={location.uuid} />
+                </MapContainer>,
+            ),
         );
     });
+
+    afterEach(() => {
+        global.fetch.mockRestore();
+    });
+
     it('should render marker without popup', () => {
         expect(document.querySelector('.leaflet-marker-icon')).toBeInTheDocument;
         expect(document.querySelector('.leaflet-popup')).not.toBeInTheDocument;
