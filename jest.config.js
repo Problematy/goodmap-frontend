a326b7c41644608c6a6d37225655d39565636d81
Krzysztof Kołodziński <krzysztof.kolodzinski@problematy.pl>
2025-09-03 22:00:17 +0200

    removed duplicated testenvironment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     setupFiles: ['<rootDir>/src/i18n'],
     verbose: true,
-    testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     collectCoverageFrom: ['src/components/**/*.jsx'],
     transform: {
