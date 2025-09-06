cd88234e2507a6dd1daf31d057db34a7169c0bba
Krzysztof Kołodziński <krzysztof.kolodzinski@problematy.pl>
2025-09-06 11:29:10 +0200

    fix after review

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
     transformIgnorePatterns: [
-        'node_modules/(?!(react-leaflet|@react-leaflet/core|react-leaflet-custom-control|@adamscybot/react-leaflet-component-marker)/)',
+        'node_modules/(?!(react-leaflet|@react-leaflet/core|react-leaflet-custom-control|@adamscybot/react-leaflet-component-marker|uuid)/)',
     ],
     moduleNameMapper: {
         '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
